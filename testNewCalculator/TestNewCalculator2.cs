3c69466947792d985f56b1d6165976a624595818
Сентемов Василий <sentemovv55@gmail.com>
2023-02-12 21:00:59 +0400

    исправление недочетов

--- testNewCalculator/TestNewCalculator2.cs
+++ testNewCalculator/TestNewCalculator2.cs
@@ -5,91 +5,111 @@ namespace testNewCalculator
 {
     internal class TestNewCalculator2
     {
-        List<double> ListNumber = new List<double>();
+        List<double> NumbersFromTheExample = new List<double>();
         List<string> ListString = new List<string>();
-        List<char> ListChar = new List<char>();
-        string[]? ArrayNumbers;
-        string? Number;
-        public string[] Read2()
-        {
-            ListString.Clear();
-            Console.WriteLine("Введите второе число");
-            Number = Console.ReadLine();
+        List<char> MathematicalSymbols = new List<char>();
+        string[]? TemporaryContainer { get; set; }
+        string? ReadIsConsole { get; set; }
 
-            ArrayNumbers = Number.Split('+', '-');
-            foreach (string number in ArrayNumbers)
+        public string[] Read()
+        {
+            MathematicalSymbols.Clear();
+            ListString.Clear();
+            Console.WriteLine("Введите пример:");
+            ReadIsConsole = Console.ReadLine();
+
+            TemporaryContainer = ReadIsConsole.Split('+', '-','*','/');
+            foreach (string number in TemporaryContainer)
             {
                 ListString.Add(number);
             }
-            foreach (char item in Number)
+            foreach (char item in ReadIsConsole)
             {
                 if (item == '+')
                 {
-                    ListChar.Add(item);
+                    MathematicalSymbols.Add(item);
                 }
                 else if (item == '-')
                 {
-                    ListChar.Add(item);
+                    MathematicalSymbols.Add(item);
+                }
+                else if(item == '*')
+                {
+                    MathematicalSymbols.Add(item);
+                }
+                else if (item == '/')
+                {
+                    MathematicalSymbols.Add(item);
                 }
             }
-            return ArrayNumbers;
+            return TemporaryContainer;
         }
 
-        public string Validator()
+        public void Validator()
         {
             int j = 0;
             int LengthArray = 1;
-            double result = -1;
             ListString.Add("");
             for (int i = 0; i < LengthArray; i++) 
             {
-                if (Number[0] != '-')
+                while (LengthArray >= i)
                 {
-                    while (LengthArray >= i)
+                    if (double.TryParse(ListString[j], out var result))
                     {
-                        if (double.TryParse(ListString[j], out result))
-                        {
-                            Console.WriteLine(ListString[i]);
-                            ListNumber.Add(result);
-                            j++;
-                            break;
-                        }
-                        else
-                        {
-                            Read2();
-                            ListString.Remove("");
-                            LengthArray = ArrayNumbers.Length;
-                            j = 0;
-                            i = 0;
-                        }
+                        NumbersFromTheExample.Add(result);
+                        j++;
+                        break;
+                    }
+                    else
+                    {
+                        Read();
+                        ListString.Remove("");
+                        LengthArray = TemporaryContainer.Length;
+                        j = 0;
+                        i = 0;
                     }
                 }
-                else if (Nmber)
-                {
-
-                }
             }
-            return Number;
         }
 
         public void WorkString()
         {
-            double otvet = 0;
-            for (int i = 0; i < ListChar.Count; i++)
+            Validator();
+            double result = NumbersFromTheExample[0];
+            for (int i = 0; i < MathematicalSymbols.Count; i++)
             {
-                if (ListChar.Count >= ListString.Count && Number[0] == '-' )
+                if (MathematicalSymbols[i] == '*')
                 {
-                    otvet = otvet - ListNumber[0];
+                    result = NumbersFromTheExample[i] * NumbersFromTheExample[i + 1];
+                    NumbersFromTheExample[i] = result;
+                    NumbersFromTheExample.RemoveAt(i + 1);
+                    MathematicalSymbols.RemoveAt(i);
+                    i -= 1;
+
                 }
-                else if (ListChar[i] == '+')
+                else if(MathematicalSymbols[i] == '/')
                 {
-                    otvet = ListNumber[i] + ListNumber[i + 1];
-                }
-                else if (ListChar[i] == '-')
-                {
-                    otvet = ListNumber[i] - ListNumber[i + 1];
+                    result = NumbersFromTheExample[i] / NumbersFromTheExample[i + 1];
+                    NumbersFromTheExample[i] = result;
+                    NumbersFromTheExample.RemoveAt(i + 1);
+                    MathematicalSymbols.RemoveAt(i);
+                    i -= 1;
                 }
             }
+            result = NumbersFromTheExample[0];
+            for (int i = 0; i < MathematicalSymbols.Count; i++)
+            {                
+                if (MathematicalSymbols[i] == '+')
+                {
+                    result = result + NumbersFromTheExample[i + 1];
+                }
+                else if (MathematicalSymbols[i] == '-')
+                {
+                    result = result - NumbersFromTheExample[i + 1];
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"{ReadIsConsole} = {result}");
         }
     }
 }
